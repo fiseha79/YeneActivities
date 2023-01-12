3c0f3fbf2f0bd4f7182572cd0404c04638f2b2df
Fiseha Abebe <famarkfish79@gmail.com>
2023-01-12 09:34:07 +0300

    Minor Cleaning

--- API/Program.cs
+++ API/Program.cs
@@ -1,35 +1,13 @@
 using Persistence;
-using Application.Activities;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Application.Core;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
 
             builder.Services.AddControllers();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            builder.Services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-
-            builder.Services.AddCors(opt => 
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-
-            builder.Services.AddMediatR(typeof(List.Handler).Assembly);
-            builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
+            builder.Services.AddApplicationServices(builder.Configuration);
 
 // configure the http request pipeline
 
